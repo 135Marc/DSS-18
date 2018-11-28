5ec879b597b3dbbbe569869b8afa8592d83a4bb7
135Marc <ricardofp9719@hotmail.com>
2018-11-28 19:01:49 +0000

    Jantes caralho

--- src/Items/Jante.java
+++ src/Items/Jante.java
@@ -1,4 +1,56 @@
 package Items;
 
-public class Jantes {
+public class Jante {
+    private String tipo;
+    private int espessura;
+
+    public Jante() {
+        this.tipo=null;
+        this.espessura=0;
+    }
+
+    public Jante(Jante js) {
+        this.tipo = js.getTipo();
+        this.espessura= js.getEspessura();
+    }
+
+    public Jante(String tipo, int espessura) {
+        this.tipo = tipo;
+        this.espessura = espessura;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getEspessura() {
+        return espessura;
+    }
+
+    public void setEspessura(int espessura) {
+        this.espessura = espessura;
+    }
+
+
+    public Jante clone() {
+        return new Jante(this);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Tipo: ")
+                .append(getTipo())
+                .append(" Espessura: ")
+                .append(getEspessura())
+                .append("''");
+                return sb.toString();
+    }
+
+    public boolean equals(Jante j) {
+        return (this.tipo.equalsIgnoreCase(j.getTipo()) && this.espessura==j.getEspessura());
+    }
 }
